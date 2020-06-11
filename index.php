9884f21c90084c3115e82b0ebfccee0785fb6acf
vsine <proulx@yeah.net>
2020-06-11 11:12:46 +0800

    update

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+<?php
 
-</body>
+$html= <<<EOT
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QVQOL</title>
+    <link rel="stylesheet" href="css/layout.css" />
+  </head>
+  <body>
+  <div class="header" style="background:#EE22ee; color:#EEE">
+</div>
+<div class="content">
+
+       
+</div>
+<div class="footer"></div>
+
+    <div class="caption">test</div>
+  </body>
 </html>
+EOT;
+
+echo $html;
